4661a20aae25ad3163b78e980ebb83fb9cbcee65
unknown <asafaviv89@gmail.com>
2018-09-02 14:12:12 +0300

    first commit

---
+++ example1/js/client.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // DOM Elements
+  const body = document.querySelector('body');
+  const nav = document.querySelector('.nav');
+  const navSignup = document.querySelector('.nav__signup');
+  const navTogglers = document.querySelectorAll('.hamburger, .nav__menu__fill');
+  const mainCards = document.querySelectorAll('.card');
+
+  // Event Listeners
+  navTogglers.forEach(toggler => toggler
+    .addEventListener('click', toggleNav));
+
+  document.querySelector('.nav__menu .nav__item:last-of-type')
+    .addEventListener('click', toggleSignup);
+
+  try {
+    mainCards.forEach((card, i) => observer((i + 1) * 100).observe(card));
+  } catch(e) {
+    mainCards.forEach(card => card.style.opacity = '1');
+  }
+
+  // Functions
+  function observer(ms) {
+    return new IntersectionObserver(function(entries) {
+      const threshold = this.thresholds[0];
+
+      if (entries[0].intersectionRatio >= threshold) {
+        entries[0].target.classList.add('slide-in', `delay-${ms}`);
+        this.unobserve(entry.target);
+      }
+    }, { threshold: 0.4 });
+  }
+
+  function toggleNav() {
+    body.style.overflow = nav.classList.contains('nav--open') ?
+    'visible' : 'hidden';
+
+    nav.classList.toggle('nav--open');
+  }
+
+  function toggleSignup(e) {
+    const classes = e.target.classList;
+
+    if (classes.contains('btn--bordered-base') || classes.contains('nav--toggler')) {
+      const navDisplay = navSignup.style.display;
+      navSignup.style.display = !navDisplay || navDisplay === 'none' ? 'block' :'none';
+    }
+  }
+});
---
+++ example2/js/client.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // DOM Elements
+  const body = document.querySelector('body');
+  const nav = document.querySelector('.nav');
+  const navTogglers = document.querySelectorAll('.hamburger, .nav__menu__fill');
+  const navLinks = nav.querySelectorAll('.nav__link');
+  const pageSections = document.querySelectorAll('.page__header, .page__header__img, .main__guide, .contact__section');
+  const pageSectionsOffsets = [...pageSections].map(sec => sec.offsetTop - 54);
+
+  // OnLoad
+  activeMenuSwitcher();
+
+  // Event Listeners
+  document.onscroll = activeMenuSwitcher;
+
+  navTogglers.forEach(toggler => toggler
+    .addEventListener('click', toggleNav));
+
+  navLinks.forEach((link, linkIndex) => link
+    .addEventListener('click', () => {
+      isNavOpen() && toggleNav();
+      smoothScroll(pageSectionsOffsets[linkIndex]);
+  }));
+
+  document.querySelector('.page__header__btn')
+    .addEventListener('click', () =>
+      smoothScroll(pageSectionsOffsets[2])
+  );
+
+  // Functions
+  const isNavOpen = () => nav.classList.contains('nav--open');
+
+  function toggleNav() {
+    body.style.overflow = nav.classList.contains('nav--open') ?
+    'visible' : 'hidden';
+
+    nav.classList.toggle('nav--open');
+  }
+
+  function smoothScroll(offsetTop) {
+    window.scrollTo({
+      top: offsetTop,
+      behavior: 'smooth'
+    });
+  }
+
+  function activeMenuSwitcher() {
+    const currPos = window.pageYOffset;
+
+    pageSectionsOffsets.forEach((secOffset, secIndex) => {
+      if (currPos >= secOffset) {
+        navLinks.forEach((link, linkIndex) => {
+          secIndex === linkIndex ?
+            link.classList.add('nav__link--active') :
+            link.classList.remove('nav__link--active');
+        });
+      }
+    });
+  };
+});
+
+
