062a356163c56dd9b6299f29f765c2d664bc90c2
unknown <asafaviv89@gmail.com>
2018-09-23 21:25:03 +0300

    Added example4

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 const gulp         = require('gulp');
 const sass         = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS     = require('gulp-clean-css');
 
 gulp.task('sass', () =>
   gulp.task('sass', () =>
     gulp.src('./**/css/*.sass')
       .pipe(sass().on('error', sass.logError))
-      .pipe(autoprefixer())
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(cleanCSS())
       .pipe(gulp.dest('./'))
   )
 );
