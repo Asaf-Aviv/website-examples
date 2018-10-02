b279bcdcad6de838b1fd5f99663cfbad8bfa9320
unknown <asafaviv89@gmail.com>
2018-10-02 19:02:08 +0300

    Finished example4

--- example4/js/client.js
+++ example4/js/client.js
@@ -1,26 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   // DOM Elements
-  const body          = document.querySelector('body');
-  const signupButton  = document.querySelector('.nav__signup');
-  const menuContainer = document.querySelector('.menu__container');
-  const navTogglers   = document.querySelectorAll('.nav--toggler, .nav__menu__fill');
+  const navMenu = document.querySelector('.nav__menu');
 
   // Event Listeners
-  navTogglers.forEach(toggler => toggler
-    .addEventListener('click', toggleNav));
-
-  signupButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    this.classList.toggle('nav__signup--active');
-  });
-
-  // Functions
-  function toggleNav() {
-    body.style.overflow = menuContainer.classList.contains('menu__container--open') ?
-      'visible' : 'hidden';
-
-    menuContainer.classList.toggle('menu__container--open');
-  }
+  document.querySelector('.nav__toggler')
+    .addEventListener('click', () => navMenu.classList.toggle('nav__menu--open'));
 });
-
-
